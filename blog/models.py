31823c6b11fd40928cfb0b1af4877b750cd57e64
rc <rchang21@gmail.com>
2017-05-08 19:27:55 -0700

    Removed approval for comments, customized views

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 		self.save()
 		
 	def approved_comments(self):
-		return self.comments.filter(approved_comment=True)
+		return self.comments
 		
 	def __str__(self):
 		return self.title
@@ -25,11 +25,6 @@ class Comment(models.Model):
 	author = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
-	approved_comment = models.BooleanField(default=False)
-	
-	def approve(self):
-		self.approved_comment = True
-		self.save()
 		
 	def __str__(self):
 		return self.text
--- blog/views.py
+++ blog/views.py
@@ -68,13 +68,7 @@ def add_comment_to_post(request, pk):
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = CommentForm()
-	return render(request, 'blog/add_comment_to_post', {'form': form})
-
-@login_required	
-def comment_approve(request, pk):
-	comment = get_object_or_404(Comment, pk=pk)
-	comment.approve()
-	return redirect('post_detail', pk=comment.post.pk)
+	return render(request, 'blog/add_comment_to_post.html', {'form': form})
 	
 @login_required
 def comment_remove(request, pk):
