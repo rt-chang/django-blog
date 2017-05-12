379e62a082112df4767e44b5f1cd8cb69db7937d
rc <rchang21@gmail.com>
2017-05-11 20:38:55 -0700

    Fixed remove comment, banner image

--- blog/models.py
+++ blog/models.py
@@ -2,29 +2,29 @@ from django.db import models
 from django.utils import timezone
 
 
-
 class Post(models.Model):
-	author = models.ForeignKey('auth.User')
-	title = models.CharField(max_length = 200)
-	text = models.TextField()
-	created_date = models.DateTimeField(default = timezone.now)
-	published_date = models.DateTimeField(blank = True, null = True)
-	
-	def publish(self):
-		self.published_date = timezone.now()
-		self.save()
-		
-	def approved_comments(self):
-		return self.comments
-		
-	def __str__(self):
-		return self.title
-		
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments
+
+    def __str__(self):
+        return self.title
+
+
 class Comment(models.Model):
-	post = models.ForeignKey('blog.Post', related_name='comments')
-	author = models.CharField(max_length=200)
-	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now)
-		
-	def __str__(self):
-		return self.text
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
