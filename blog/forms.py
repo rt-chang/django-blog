b9b23dcf6455ea9af30c04114e6a70f38b122e7a
rc <rchang21@gmail.com>
2017-05-14 01:12:17 -0700

    Resume layout complete. Beginning sidebar

--- blog/forms.py
+++ blog/forms.py
@@ -2,13 +2,21 @@ from django import forms
 
 from .models import Post, Comment
 
+
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ('title', 'text',)
-		
+
+
 class CommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment
 		fields = ('author', 'text',)
-		
+
+
+class ContactForm(forms.Form):
+	contact_name = forms.CharField(required=True, label='Your Name')
+	contact_email = forms.EmailField(required=True, label='Your Email')
+	subject = forms.CharField(max_length=100)
+	message = forms.CharField(required=True, widget=forms.Textarea)
